e5cd0a4b590bd0a8f4f0ec3f17378e2c7bdef52d
JasonPhan-ship-it <jasonphuocphan@gmail.com>
2025-08-05 15:32:41 -0700

    Update middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,9 +1,5 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware();
-
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-  runtime: "nodejs",
 };
+
 
