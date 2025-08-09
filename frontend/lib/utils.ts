94f92dcbc594e9354f1df47540ea3ce3d16df4c6
JasonPhan-ship-it <jasonphuocphan@gmail.com>
2025-08-09 16:50:02 -0700

    Update utils.ts

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,9 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+// lib/utils.ts
+import { type ClassValue } from "clsx";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
 
 export function formatPrice(price: number): string {
   return new Intl.NumberFormat('en-US', {
