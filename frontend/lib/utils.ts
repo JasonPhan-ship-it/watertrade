0790e1e8edb791e9286c5126ce3c701c4b4e68d0
JasonPhan-ship-it <jasonphuocphan@gmail.com>
2025-08-09 17:03:05 -0700

    Update utils.ts

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,4 +1,3 @@
-// lib/utils.ts
 import { type ClassValue } from "clsx";
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
@@ -6,23 +5,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-
-export function formatPrice(price: number): string {
-  return new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format(price)
-}
-
-export function formatDate(date: string | Date): string {
-  return new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  }).format(new Date(date))
-}
-
-export function formatQuantity(quantity: number, unit: string): string {
-  return `${quantity.toLocaleString()} ${unit}`
-} 
