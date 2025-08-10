09f433305b949da965c9a59e4c6fd793e5a80468
JasonPhan-ship-it <jasonphuocphan@gmail.com>
2025-08-09 17:22:51 -0700

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,12 @@
-import type { AppProps } from "next/app";
-import "@/app/globals.css";
+// pages/_app.tsx
+import type { AppProps } from 'next/app'
+import { ClerkProvider } from '@clerk/nextjs'
+import '@/app/globals.css' // use the same global stylesheet as the app router
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
 }
