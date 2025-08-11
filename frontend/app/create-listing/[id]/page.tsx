82f47958d1177f7bb6be9250ff3a6e5f55eb3e1c
JasonPhan-ship-it <jasonphuocphan@gmail.com>
2025-08-11 13:41:10 -0700

    Update page.tsx

--- frontend/app/create-listing/[id]/page.tsx
+++ frontend/app/create-listing/[id]/page.tsx
@@ -1,4 +1,4 @@
-import prisma from "@/lib/prisma"; // Option B path
+import prisma from "@/lib/prisma";
 import BuyNow from "./parts/BuyNow";
 import MakeOffer from "./parts/MakeOffer";
 import AuctionBid from "./parts/AuctionBid";
