89f4063ca8e150d461705ac7871b788a507e2819
JasonPhan-ship-it <jasonphuocphan@gmail.com>
2025-08-14 15:43:27 -0700

    Update Navigation.tsx

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -30,7 +30,7 @@ export default function Navigation() {
                 alt="Water Traders"
                 width={1080}
                 height={480}
-                className="w-64 sm:w-72 md:w-96 h-auto -ml-20"
+                className="w-64 sm:w-72 md:w-96 h-auto -ml-25"
               />
             </Link>
           </div>
