8ce22fa596fb6522bafd4bb7054ddfd5d9649c75
JasonPhan-ship-it <jasonphuocphan@gmail.com>
2025-10-14 11:46:40 -0700

    Update Navigation.tsx

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -139,10 +139,7 @@ export default function Navigation() {
                 )}
 
                 {/* Sign out */}
-                <SignOutButton
-                  signOutCallback={() => router.push("/")}
-                  signOutOptions={{ redirectUrl: "/" }}
-                >
+                  <SignOutButton signOutCallback={() => router.push("/")}>
                   <Button variant="outline" className="px-3 py-2 text-sm">
                     <LogOut className="w-4 h-4 mr-2" />
                     Sign Out
