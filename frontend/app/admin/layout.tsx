201fbb3b50b56e226e7754fdb28cb10fb20adb00
JasonPhan-ship-it <jasonphuocphan@gmail.com>
2025-10-28 13:13:50 -0700

    Update layout.tsx

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -67,6 +67,7 @@ export default async function AdminRootLayout({ children }: { children: ReactNod
               <NavLink href="/admin/users">Users</NavLink>
               <NavLink href="/admin/transactions">Transactions</NavLink>
               <NavLink href="/admin/water">Water</NavLink>
+              <NavLink href="/admin/homepage">Homepage</NavLink>
               <NavLink href="/admin/settings">Settings</NavLink>
             </nav>
           </aside>
